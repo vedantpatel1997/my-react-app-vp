fd2992f9fc5f03046d04f223620c2c947b4dd585
vedantpatel1997 <vedantp9@gmail.com>
2024-04-30 12:59:04 -0600

    git init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>React App Vedant Patel</h1>
+        <p>Hosted through Azure Pipeline CICD</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
